ebd571a7905943eff6a3a8eaa12ede48ebd383ae
Ilia Borovitinov <ilia@electric-sql.com>
2023-10-20 14:29:46 +0300

    Added more functions

--- components/electric/lib/electric/replication/eval/env/known_functions.ex
+++ components/electric/lib/electric/replication/eval/env/known_functions.ex
@@ -19,11 +19,31 @@ defmodule Electric.Replication.Eval.Env.KnownFunctions do
   defpostgres "- *numeric_type* -> *numeric_type*", delegate: &Kernel.-/1
   defpostgres "*numeric_type* + *numeric_type* -> *numeric_type*", delegate: &Kernel.+/2
   defpostgres "*numeric_type* - *numeric_type* -> *numeric_type*", delegate: &Kernel.-/2
-  defpostgres "*numeric_type* > *numeric_type* -> bool", delegate: &:erlang.>/2
-  defpostgres "*numeric_type* < *numeric_type* -> bool", delegate: &:erlang.</2
+  defpostgres "*numeric_type* > *numeric_type* -> bool", delegate: &Kernel.>/2
+  defpostgres "*numeric_type* >= *numeric_type* -> bool", delegate: &Kernel.>=/2
+  defpostgres "*numeric_type* < *numeric_type* -> bool", delegate: &Kernel.</2
+  defpostgres "*numeric_type* <= *numeric_type* -> bool", delegate: &Kernel.<=/2
+  defpostgres "*integral_type* / *integral_type* -> bool", delegate: &Kernel.div/2
+  defpostgres "float8 / float8 -> bool", delegate: &Kernel.//2
+  defpostgres "numeric ^ numeric -> numeric", delegate: &Float.pow/2
+  defpostgres "float8 ^ float8 -> float8", delegate: &Float.pow/2
+  defpostgres "|/ float8 -> float8", delegate: &:math.sqrt/1
+  defpostgres "@ *numeric_type* -> *numeric_type*", delegate: &:erlang.abs/1
+  defpostgres "*integral_type* & *integral_type* -> *integral_type*", delegate: &Bitwise.band/2
+  defpostgres "*integral_type* | *integral_type* -> *integral_type*", delegate: &Bitwise.bor/2
+  defpostgres "*integral_type* # *integral_type* -> *integral_type*", delegate: &Bitwise.bxor/2
 
   ## String functions
 
+  defpostgres "text || text -> text" do
+    # Can't do `delegate: &Kernel.<>/2`, because it's a macro that gets converted to local function call
+    def text_concat(t1, t2) when is_binary(t1) and is_binary(t2) do
+      <<t1::binary, t2::binary>>
+    end
+  end
+
+  defpostgres "lower(text) -> text", delegate: &String.downcase/1
+  defpostgres "upper(text) -> text", delegate: &String.upcase/1
   defpostgres "text ~~ text -> bool", delegate: &Casting.like?/2
   defpostgres "text ~~* text -> bool", delegate: &Casting.ilike?/2
 
