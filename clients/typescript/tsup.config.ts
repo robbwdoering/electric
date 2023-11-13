7cc940e54a1b80831a8eb7f5d4d12883eaaed176
Sam Willis <sam.willis@gmail.com>
2023-11-13 19:26:09 +0000

    Swap to tsup for building client

---
+++ clients/typescript/tsup.config.ts
@@ -0,0 +1,24 @@
+import packageJson from './package.json'
+import { defineConfig } from 'tsup'
+
+// Entry points from package.json
+let entry = Object.values(packageJson.exports)
+  .filter((path) => path.startsWith('./dist/') && path.endsWith('.js'))
+  .map((path) => path.replace('./dist/', './src/').replace('.js', '.ts'))
+
+// Add entry points from typesVersions
+Object.values(packageJson.typesVersions['*']).map((paths: string[]) => {
+  paths.map((path) => {
+    entry.push(path.replace('./dist/', './src/').replace('.d.ts', '.ts'))
+  })
+})
+
+entry = [...new Set(entry)] // Remove duplicates
+
+export default defineConfig({
+  entry: entry,
+  format: ['esm'],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+})
