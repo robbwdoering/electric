914b494f15f0938ba996d2bedc66dff5780bb293
tachyonicbytes <support@tachyonicbytes.com>
2023-12-05 21:01:36 +0200

    Command worked for GNU sed, but not BSD sed. Corrected, in order to build on macOS

--- examples/tauri-postgres/patch.sh
+++ examples/tauri-postgres/patch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # This is a script that solves inconsistencies for the tauri linearlite demo
-# At the end, this file should be empty/deleted
+# At the end of the demo, this file should be empty
 
-sed -i 's/WITHOUT ROWID//g' src/generated/client/migrations.ts
+# sed -i 's/WITHOUT ROWID//g' src/generated/client/migrations.ts
+# sed -i '' 's/WITHOUT ROWID//g' src/generated/client/migrations.ts
+sed 's/WITHOUT ROWID//g' src/generated/client/migrations.ts > tmpfile && mv tmpfile src/generated/client/migrations.ts
